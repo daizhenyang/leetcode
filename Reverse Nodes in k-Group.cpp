7d39bd7630824f105a3ccb61bbb25f0e8b168cbc
daizhenyang <daizhenyang@gmail.com>
2013-08-31 14:35:38 -0400

    Update Reverse Nodes in k-Group.cpp

--- Reverse Nodes in k-Group.cpp
+++ Reverse Nodes in k-Group.cpp
@@ -41,3 +41,60 @@ public:
         
     }
 };
+
+
+//non recursion
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reverse(ListNode *head)
+    {
+        ListNode *ptr=head,*tmp=NULL;
+        while (ptr)
+        {
+            ListNode *nxt=ptr->next;
+            ptr->next=tmp;
+            tmp=ptr;
+            ptr=nxt;
+        }
+        return tmp;
+    }
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (head==NULL)return head;
+        ListNode *ptr=head,*last=NULL,*rt=NULL;
+        while (ptr)
+        {
+            int c=k;
+            ListNode *ed=ptr;
+            while (--c&&ed)
+            {
+                ed=ed->next;
+            }
+            if (!ed)
+            {
+                if (last)last->next=ptr;
+                break;
+            }
+            ListNode *nxt=ed->next;
+            ed->next=NULL;
+            ListNode *pre=ptr;
+            ptr=reverse(ptr);
+            if (last)last->next=ptr;
+            if (!rt)rt=ptr;
+            last=pre;
+            ptr=nxt;
+        }
+        if (!rt)rt=head;
+        return rt;
+        
+    }
+};
